2d1303a422f5501ee7fb5b8a557da8660ff2c092
Sedakat <sedakatekmekci16@gmail.com>
2023-01-17 15:02:59 +0300

    Update app.js
    Pulling post data

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ import axios from "axios";
 //console.log("success");
 async function getData(number) {
 
-    const { data: users } = await axios('https://jsonplaceholder.typicode.com/users/1'); //pulling user data 
-    console.log(users);
+    const { data: user } = await axios('https://jsonplaceholder.typicode.com/users/1'); //pulling user data 
+    console.log(user);
+
+    const { data: post } = await axios('https://jsonplaceholder.typicode.com/posts/1'); //pulling post data
+    console.log("Posts", post);
 };
 
 getData(1);
