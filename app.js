51be262b3aecfc26edcc04e31f282812b2327c07
Sedakat <sedakatekmekci16@gmail.com>
2023-01-17 14:56:07 +0300

    Update app.js
    pulling user data

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 import axios from "axios";
+//console.log("success");
+async function getData(number) {
 
-const getData = (user_id) => {
-    return new Promise(async (resolve, reject) => {
-        const { data } = await axios('https://jsonplaceholder.typicode.com/users' + user_id);  //pulling user data
-
-        resolve(console.log(data));
-        reject(error);
-    });
+    const { data: users } = await axios('https://jsonplaceholder.typicode.com/users/1'); //pulling user data 
+    console.log(users);
 };
 
-const getPost = (post_id) => {
-    return new Promise(async (resolve, reject) => {
-        const { data } = await axios('https://jsonplaceholder.typicode.com/posts' + post_id); //pulling post data
-
-        resolve(console.log(data));
-        reject(error);
-    })
-};
-
+getData(1);
